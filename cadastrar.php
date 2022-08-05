7aa480e5e44aee36c4ae28abeb346bfbc21633be
Gabriel-Scopel <gabriel.souza.scopel@hotmail.com>
2022-08-04 22:04:13 -0300

    update

--- cadastrar.php
+++ cadastrar.php
@@ -1,4 +1,27 @@
 <?php
+require_once('class/config.php');
+require_once('autoload.php');
+
+if(isset($_POST['nome']) && isset ($_POST['email']) && isset($_POST['senha']) && isset($_POST['repete_senha'])){
+    $nome = limpaPost($_POST['nome']);
+    $email = limpaPost($_POST['email']);
+    $senha = limpaPost($_POST['senha']);
+    $repete_senha = limpaPost($_POST['repete_senha']);
+    if(empty($nome) or empty($email) or empty($senha) or empty($repete_senha) or empty($_POST['termos'])){
+        $termo_geral = "todos os campos são obrigatórios!";
+    }else{
+        $usuario = new Usuario($nome, $email, $senha);
+        $usuario->set_repeticao($repete_senha);
+        $usuario->validar_cadastro();
+        if(empty($usuario->erro)){
+            if($usuario->insert()){
+                header("location: index.php");
+            }else{
+                $erro_geral = $usuario->erro["erro_geral"];
+            }
+        }
+    }
+}
 
 ?>
 <!DOCTYPE html>
@@ -17,40 +40,48 @@
 <body>
     <form method="POST">
         <h1>Cadastrar</h1>
+
+        <?php if(isset($erro_geral)){?>
+            <div class="erro-geral animate__animated animate__rubberBand">
+                <?php echo$erro_geral;?>
+            </div>
+        <?php } ?>
          
-        <div class="erro-geral animate__animated animate__rubberBand">
-            Aqui vai o erro para o usuário
-        </div>
+        
 
         <div class="input-group">
             <img class="input-icon" src="img/card.png">
-            <input class="erro-input" name="nome" type="text" placeholder="Nome Completo" required>
-            <div class="erro">Por favor informe um nome válido!</div>
+            <input <?php if(isset($usuario->erro["erro_nome"]) or isset($erro_geral)){echo 'class="erro-input"';}?>name="nome" type="text" placeholder="Nome Completo" required>
+            <div class="erro"><?php if (isset($usuario->erro["erro_nome"])){ $usuario->erro["erro_nome"];}?></div>
         </div>
 
         <div class="input-group">
             <img class="input-icon" src="img/user.png">
-            <input type="email" name="email" placeholder="Seu melhor email" required>
+            <input <?php if(isset($usuario->erro["erro_email"]) or isset($erro_geral)){echo 'class="erro-input"';}?> type="email" name="email" placeholder="Seu melhor email" required>
+            <div class="erro"><?php if (isset($usuario->erro["erro_email"])){ $usuario->erro["erro_email"];}?></div>
         </div>
 
         <div class="input-group">
             <img class="input-icon" src="img/lock.png">
-            <input type="password" name="senha" placeholder="Senha mínimo 6 Dígitos" required>
+            <input <?php if(isset($usuario->erro["erro_senha"]) or isset($erro_geral)){echo 'class="erro-input"';}?> type="password" name="senha" placeholder="Senha mínimo 6 Dígitos" required>
+            <div class="erro"><?php if (isset($usuario->erro["erro_senha"])){ $usuario->erro["erro_senha"];}?></div>
         </div>
 
         <div class="input-group">
             <img class="input-icon" src="img/lock-open.png">
-            <input type="password" name="repete_senha" placeholder="Repita a senha criada" required>
+            <input <?php if(isset($usuario->erro["erro_repete"]) or isset($erro_geral)){echo 'class="erro-input"';}?> type="password" name="repete_senha" placeholder="Repita a senha criada" required>
+            <div class="erro"><?php if (isset($usuario->erro["erro_repete"])){ $usuario->erro["erro_repete"];}?></div>
         </div>   
         
-        <div class="input-group">
+        <div <?php if(isset($erro_gerla) && $erro_geral == "todos os campos são obrigatórios!"){echo 'class="input-group erro-input"';}else{echo 'class="input-group" ';}?>>
+
             <input type="checkbox" id="termos" name="termos" value="ok" required>
             <label for="termos">Ao se cadastrar você concorda com a nossa <a class="link" href="#">Política de Privacidade</a> e os <a class="link" href="#">Termos de uso</a></label>
         </div>  
        
         
         <button class="btn-blue" type="submit">Cadastrar</button>
-        <a href="index.html">Já tenho uma conta</a>
+        <a href="index.php">Já tenho uma conta</a>
     </form>
 </body>
 </html>
--- class/Usuario.php
+++ class/Usuario.php
@@ -1,7 +1,7 @@
 <?php
 require_once('Crud.php');
 
-class Usuario extends Crud{
+abstract class Usuario extends Crud{
     protected string $tabela = 'usuarios';
     function __construct(
         public string $nome,
@@ -14,4 +14,43 @@ class Usuario extends Crud{
         private string $status="",
         public array $erro=[]
     ){}
+
+    public function set_repeticao($repete_senha){
+        $this->repete_senha = $repete_senha;
+    }
+    public function validar_cadastro(){
+        if (!preg_match("/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\s]+$/",$this->nome)) {
+            $this->erro["erro_nome"] = "Por favor, informe um nome válido";}
+        if(!filter_var($this->email, FILTER_VALIDADE_EMAIL)){
+            $this->erro["erro_email"] = "Email inválido";
+        }
+        if(strlen($this->senha)<6){
+            $this->erro["erro_senha"] = "A senha deve ter 6 caracteres ou mais";
+        }
+        if($this->senha !== $this->repete_senha){
+            $this->erro["erro_repete"] = "A senha deve ser igual a anterior";
+        }
+    
+    }
+    public function insert(){
+        $sql = "SELECT * FROM usuario WHERE email=? LIMIT 1";
+        $sql = DB::prepare($sql);
+        $sql->execute(array($this->email));
+        $usuario = $sql->fetch();
+        if(!$usuario){
+            $data_cadastro = date('d/m/Y');
+            $senhacripto = sha1($this->senha);
+            $sql = "INSET INTO $this->tabela VALUES(null,?,?,?,?,?,?,?,?)";
+            $sql = DB::prepare($sql);
+            return $sql->execute(array($this->nome,$this->email,$senhacripto,$this->recupera_senha,$this->token,$this->codigo_confirmacao,$this->status,$data_cadastro));
+        }else{
+            $this->erro["erro_geral"] = "Usuario já cadastrado";
+        }
+    }
+    public function update($id){
+        $sql = "UPDATE $this->tabela SET token=? WHERE id=?";
+        $sql = DB::prepare($sql);
+        return $sql->execute(array($token, $id));
+    }
+
 }
